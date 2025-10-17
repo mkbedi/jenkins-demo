e2a014696ac7a34b4199f4a424083dcbb9d11dcb
manpreet.kaur <manpreet.kaur@tothenew.com>
2025-10-17 17:45:28 +0530

    added node js job dsl basic

---
+++ jenkins/jobs/nodejs-pipeline-job.groovy
@@ -0,0 +1,24 @@
+job('nodejs-basic-job') {
+    description('A basic Node.js build job created using Jenkins Job DSL.')
+
+    scm {
+        git('https://github.com/mkbedi/jenkins-demo.git', 'main')
+    }
+
+    triggers {
+        scm('H/5 * * * *')  // Poll every 5 minutes for changes
+    }
+
+    wrappers {
+        nodejs('NodeJs 25')  // Use Jenkins NodeJS tool (configure this in Jenkins → Global Tool Configuration)
+    }
+
+    steps {
+        shell('npm install')
+        shell('npm test')
+    }
+
+    publishers {
+        archiveArtifacts('**/test-results/*.xml')
+    }
+}
