aae56bd8d6a16c4ec02208703aba02b0b6af7a32
manpreet.kaur <manpreet.kaur@tothenew.com>
2025-10-17 18:02:58 +0530

    added new job dsl throgh jenkins file

---
+++ jenkins/jobs/nodejs_jenkins_pipeline_job.groovy
@@ -0,0 +1,25 @@
+pipelineJob('nodejs-pipeline-job') {
+    description('A pipeline-based Node.js CI/CD job created with Jenkins Job DSL.')
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/mkbedi/jenkins-demo.git')
+                    }
+                    branch('main')
+                }
+            }
+            scriptPath('Jenkinsfile') // Jenkinsfile path inside the repo
+        }
+    }
+
+    triggers {
+        scm('H/5 * * * *')  // Poll every 5 minutes for Git changes
+    }
+
+    properties {
+        disableConcurrentBuilds()   // Prevent overlapping runs
+    }
+}
